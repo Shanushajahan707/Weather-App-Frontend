f797ccac8c6e6f5d0f507e793b8cb90ed60e941b
Shanushajahan707 <shanushajahan707@gmail.com>
2024-08-07 16:08:27 +0530

    after hosting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/user/'
+    // apiUrl: 'http://localhost:3000/user/'
+    apiUrl: 'https://weather-touch.onrender.com/user/'
   };
