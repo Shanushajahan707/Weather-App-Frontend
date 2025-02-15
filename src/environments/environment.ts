7eddd63980d2079f75fa4177d777220c26e8eda8
Shanushajahan707 <shanushajahan707@gmail.com>
2025-02-15 09:59:04 +0530

    url changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/user/'
-    // apiUrl: 'https://weather-touch.onrender.com/user/'
+    // apiUrl: 'http://localhost:3000/user/'
+    apiUrl: 'https://weather-touch.onrender.com/user/'
   };
